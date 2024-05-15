658bb612e364a71db6508c63faf1443a8da3d511
Zakaria el Barnoussi <zakaria@rubberducktech.nl>
2024-05-15 11:36:25 +0200

    Added cypher agent using connect to master method

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 # Docker build images
 docker-compose build jenkins-controller
 docker-compose build jenkins-ssh-agent
+docker-compose build jenkins-connectto-agent
 docker-compose build openldap
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,6 @@
-# Docker build images
-docker-compose build jenkins-controller
-docker-compose build jenkins-ssh-agent
-docker-compose build openldap
-
 # Start k8s
 kubectl apply -f .k8s/
 kubectl apply -f .k8s/openldap
 kubectl apply -f .k8s/jenkins-agents/ssh
+kubectl apply -f .k8s/jenkins-agents/connectto
 kubectl apply -f .k8s/jenkins-controller
